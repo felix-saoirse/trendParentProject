0efbe45e49cc7001ea5316f3f3a3874f096e61df
yzn_f <yzn_felix@163.com>
2022-10-04 21:09:27 +0800

    第五次测试提交 2022年10月4日21:08:14 更新： 模拟回测-指数基础数据 模拟回测-切换指数 模拟回测-日期 模拟回测-收益对比图 模拟回测-交易明细

---
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/pojo/Profit.java
@@ -0,0 +1,25 @@
+package cn.how2j.trend.pojo;
+
+/**
+ * @Description : TODO
+ * @Author : yzn_f
+ * @Date : 2022/10/4 20:23
+ * @Version : 1.0
+ **/
+public class Profit {
+    String date;
+    float value;
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public float getValue() {
+        return value;
+    }
+    public void setValue(float value) {
+        this.value = value;
+    }
+}
--- trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
@@ -1,16 +1,18 @@
 package cn.how2j.trend.web;
 
 import cn.how2j.trend.pojo.IndexData;
+import cn.how2j.trend.pojo.Profit;
+import cn.how2j.trend.pojo.Trade;
 import cn.how2j.trend.service.BackTestService;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Description : TODO
@@ -31,12 +33,58 @@ public class BackTestController {
      * @param code
      * @return java.util.Map<java.lang.String,java.lang.Object>
      */
-    @GetMapping("/simulate/{code}")
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     @CrossOrigin
-    public Map<String,Object> backTest(@PathVariable("code") String code) throws Exception {
+    public Map<String,Object> backTest(
+            @PathVariable("code") String code
+            //参数可以接受开始日期和结束日期
+            ,@PathVariable("startDate") String strStartDate
+            ,@PathVariable("endDate") String strEndDate
+    ) throws Exception {
+        //计算出开始日期和结束日期并返回
         List<IndexData> allIndexDatas = backTestService.listIndexData(code);
+
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size()-1).getDate();
+
+        //根据开始日期和结束日期获取对应日期范围的数据
+        allIndexDatas = filterByDateRange(allIndexDatas,strStartDate, strEndDate);
+
+        int ma = 20;
+        float sellRate = 0.95f;
+        float buyRate = 1.05f;
+        float serviceCharge = 0f;
+        Map<String,?> simulateResult= backTestService.simulate(ma,sellRate, buyRate,serviceCharge, allIndexDatas);
+        List<Profit> profits = (List<Profit>) simulateResult.get("profits");
+        List<Trade> trades = (List<Trade>) simulateResult.get("trades");
+
         Map<String,Object> result = new HashMap<>();
         result.put("indexDatas", allIndexDatas);
+        result.put("indexStartDate", indexStartDate);
+        result.put("indexEndDate", indexEndDate);
+        result.put("profits", profits);
+        result.put("trades", trades);
+        return result;
+    }
+
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        if(StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate) ){
+            return allIndexDatas;
+        }
+
+        List<IndexData> result = new ArrayList<>();
+        Date startDate = DateUtil.parse(strStartDate);
+        Date endDate = DateUtil.parse(strEndDate);
+
+        for (IndexData indexData : allIndexDatas) {
+            Date date =DateUtil.parse(indexData.getDate());
+            if(
+                    date.getTime()>=startDate.getTime() &&
+                            date.getTime()<=endDate.getTime()
+            ) {
+                result.add(indexData);
+            }
+        }
         return result;
     }
 }
